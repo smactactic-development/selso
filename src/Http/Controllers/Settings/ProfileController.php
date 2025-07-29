cf716587019cb024a1988ccf8e1003ed63276dba
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-07-29 11:02:41 +0700

    patch: adjust config

--- src/Http/Controllers/Settings/ProfileController.php
+++ src/Http/Controllers/Settings/ProfileController.php
@@ -11,14 +11,14 @@ class ProfileController extends Controller
     public function edit()
     {
         $payload['sub'] = (int)Auth::id();
-        $payload['client_id'] = (int)config('sso.client_id');
+        $payload['client_id'] = (int)config('selso.client_id');
         $payload['exp'] = (int)now()->addMinutes(1)->timestamp;
 
-        $secret = config('sso.client_secret') . config('sso.public_key');
+        $secret = config('selso.client_secret') . config('selso.public_key');
         $code = JWT::encode($payload, $secret, 'HS256');
 
         $query = http_build_query(['code' => $code]);
 
-        return redirect(config('sso.auth_server_url') . '/settings/profile/direct?' . $query);
+        return redirect(config('selso.auth_server_url') . '/settings/profile/direct?' . $query);
     }
 }
